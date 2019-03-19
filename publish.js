721a38f8caad4cf2abeb256f33c5b11d36500d1d
Hernan Zhou <hernanzh@gmail.com>
2019-03-19 16:54:21 +0100

    fix fs.copyFileSync for NodeJS 10.13+

--- publish.js
+++ publish.js
@@ -474,7 +474,7 @@ exports.publish = function(taffyData, opts, tutorials) {
     staticFiles.forEach(function(fileName) {
         var toDir = fs.toDir( fileName.replace(fromDir, outdir) );
         fs.mkPath(toDir);
-        fs.copyFileSync(fileName, toDir);
+        fs.copyFileSync(fileName, path.join(toDir, path.basename(fileName)));
     });
 
     // copy user-specified static files to outdir
